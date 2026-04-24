9a7f3e17bb18b425617b6e2c5a5432779f498a6f
dzdzFa <nikolaenkovdan@gmail.com>
2026-04-24 12:29:09 +0300

    Добавил новых файлов и исправил мэин джс

---
+++ src/components/Detail.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>dddddd</h1>
+</template>
---
+++ src/router/router.js
@@ -0,0 +1,27 @@
+import { creatRouter, createWebHistory } from "vue-router";
+import Home from "../components/Home.vue";
+import Detail from "../components/Detail.vue";
+import Tape from "../components/Tape.vue";
+import NotFound from "../components/NotFound.vue";
+import { Component } from "react";
+
+const routes = [
+  { path: "/", name: "home", Component: Home },
+  { path: "/track/:id", name: "detail", Component: Detail },
+  {
+    path: "/tape",
+    name: "tape",
+    Component: Tape,
+    meta: { requiresSave: true },
+  },
+  { path: "/:pathMatch(.*)*", name: "not-found", Component: NotFound },
+];
+
+const router = creatRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+router.beforeEach((to, from, next) => next());
+
+export default router;
